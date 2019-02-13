179d6bda6999a35a1987de69fdfa32d66c785269
Chad Roesler <ChadRoesler@Me.com>
2019-02-13 12:01:32 -0600

    Update AssemblyInfo.cs
    Minor update

--- PerforcePuller/src/PerforcePuller/Properties/AssemblyInfo.cs
+++ PerforcePuller/src/PerforcePuller/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PerforcePuller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cli to easily pull files from a Perforce Server")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PerforcePuller")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Chad Roesler 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
